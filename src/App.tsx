38cbe45da6eeae5689766d82f0e601551bc983d1
euphoriagmch2026 <euphoriagmch2026@gmail.com>
2026-02-08 11:25:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Index from "./pages/Index";
 import Events from "./pages/Events";
 import Passes from "./pages/Passes";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/events" element={<Events />} />
